2d5d9781f4286c969ebd366dd46f12c304f0e24a
Filip PC <filipsolecki@gmail.com>
2024-06-06 14:20:10 +0200

    add header component

---
+++ public/views/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="public/css/header.css">
+</head>
+
+<body>
+  <header class="header">
+    <div class="logo">
+      <img src="public/assets/images/logo.jpg" alt="Logo">
+    </div>
+    <div class="profile">
+      <i class="fas fa-user profile-icon" id="profile-icon"></i>
+      <div class="dropdown-menu" id="dropdown-menu">
+        <form action="/logout" method="POST" class="logout-form">
+          <button type="submit" class="logout-button text-xl">
+            Logout
+          </button>
+        </form>
+      </div>
+    </div>
+  </header>
+</body>
+
+</html>
